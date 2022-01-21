3db1893dda914b55a866ba5e08c620a0aef47ae4
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-21 13:16:16 -0800

    Add Swagger

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 using AnimalShelter.Models;
 
 namespace AnimalShelter
@@ -23,10 +27,24 @@ namespace AnimalShelter
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelter.Solution", Version = "v1" });
-            // });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { 
+                    Version = "v1",
+                    Title = "Animal Shelter API",
+                    Description = "An API for an Animal Shelter. This project was created during week 13 at Epicodus for an independent project.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Elizabeth Thomas",
+                        Email = "thomas.elizabeth.k@gmail.com",
+                        Url = new Uri("https://github.com/ekthomas25")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT License",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    }});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,8 +53,12 @@ namespace AnimalShelter
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter.Solution v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Animal Shelter API v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             // app.UseHttpsRedirection();
